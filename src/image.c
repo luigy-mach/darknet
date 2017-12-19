a5bb924f57896a077ab1a9cf605ec8c13e2e7e4b
luigy-mach <luigy.mach.arc@gmail.com>
2017-12-19 17:00:38 -0500

    update

--- src/image.c
+++ src/image.c
@@ -259,62 +259,65 @@ void draw_detections(image im, int num, float thresh, box *boxes, float **probs,
                     printf("%s: %.0f%%\n", names[j], probs[i][j]*100);
             }
         }
+        char strtemp[]="person";
+        if(0==strcmp(names[j],strtemp))
+        {
+            if(class >= 0){
+                //int width = im.h * .1006; //test grosor linea recuadro (box)
+                int width = im.h * .006;
 
-        if(class >= 0){
-            //int width = im.h * .006;
-            int width = im.h * .1006;
+                /*
+                   if(0){
+                   width = pow(prob, 1./2.)*10+1;
+                   alphabet = 0;
+                   }
+                 */
 
-            /*
-               if(0){
-               width = pow(prob, 1./2.)*10+1;
-               alphabet = 0;
-               }
-             */
+                //printf("%d %s: %.0f%%\n", i, names[class], prob*100);
+                int offset  = class*123457 % classes;
+                float red   = get_color(2,offset,classes);
+                float green = get_color(1,offset,classes);
+                float blue  = get_color(0,offset,classes);
+                float rgb[3];
 
-            //printf("%d %s: %.0f%%\n", i, names[class], prob*100);
-            int offset  = class*123457 % classes;
-            float red   = get_color(2,offset,classes);
-            float green = get_color(1,offset,classes);
-            float blue  = get_color(0,offset,classes);
-            float rgb[3];
+                //width = prob*20+2;
 
-            //width = prob*20+2;
+                rgb[0] = red;
+                rgb[1] = green;
+                rgb[2] = blue;
+                box b = boxes[i];
 
-            rgb[0] = red;
-            rgb[1] = green;
-            rgb[2] = blue;
-            box b = boxes[i];
+                int left  = (b.x-b.w/2.)*im.w;
+                int right = (b.x+b.w/2.)*im.w;
+                int top   = (b.y-b.h/2.)*im.h;
+                int bot   = (b.y+b.h/2.)*im.h;
 
-            int left  = (b.x-b.w/2.)*im.w;
-            int right = (b.x+b.w/2.)*im.w;
-            int top   = (b.y-b.h/2.)*im.h;
-            int bot   = (b.y+b.h/2.)*im.h;
+                if(left < 0) left = 0;
+                if(right > im.w-1) right = im.w-1;
+                if(top < 0) top = 0;
+                if(bot > im.h-1) bot = im.h-1;
 
-            if(left < 0) left = 0;
-            if(right > im.w-1) right = im.w-1;
-            if(top < 0) top = 0;
-            if(bot > im.h-1) bot = im.h-1;
+                //dibuja todos los rectagulos
+                draw_box_width(im, left, top, right, bot, width, red, green, blue);
 
-            //dibuja todos los rectagulos
-            draw_box_width(im, left, top, right, bot, width, red, green, blue);
+                //dibuja todos las etiquetas
+                if (alphabet) {
+                    image label = get_label(alphabet, labelstr, (im.h*.03)/10);
+                    draw_label(im, top + width, left, label, rgb);
+                    free_image(label);
+                }
 
-            //dibuja todos las etiquetas
-            if (alphabet) {
-                image label = get_label(alphabet, labelstr, (im.h*.03)/10);
-                draw_label(im, top + width, left, label, rgb);
-                free_image(label);
-            }
-
-            //float **masks
-            //      **masks = 0
-            if (masks){
-                image mask         = float_to_image(14, 14, 1, masks[i]);
-                image resized_mask = resize_image(mask, b.w*im.w, b.h*im.h);
-                image tmask        = threshold_image(resized_mask, .5);
-                embed_image(tmask, im, left, top);
-                free_image(mask);
-                free_image(resized_mask);
-                free_image(tmask);
+                //float **masks
+                //      **masks = 0
+                if (masks){
+                    image mask         = float_to_image(14, 14, 1, masks[i]);
+                    image resized_mask = resize_image(mask, b.w*im.w, b.h*im.h);
+                    image tmask        = threshold_image(resized_mask, .5);
+                    embed_image(tmask, im, left, top);
+                    free_image(mask);
+                    free_image(resized_mask);
+                    free_image(tmask);
+                }
             }
         }
     }
