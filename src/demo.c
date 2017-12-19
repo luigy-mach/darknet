0b581b9a9d9e770823cb4163e209b8f0842a710a
luigy-mach <luigy.mach.arc@gmail.com>
2017-12-19 17:56:44 -0500

    update

--- src/demo.c
+++ src/demo.c
@@ -66,8 +66,8 @@ void *detect_in_thread(void *ptr)
     printf("\nFPS:%.1f\n",fps);
     printf("Objects:\n\n");
     image display = buff[(buff_index+2) % 3];
-    printf("demo_detections: %d", demo_detections );
-    //printf("tamano demo_names: %s", sizeof(demo_names)/sizeof(char*) );
+    printf("demo_detections: %d \n", demo_detections );
+    printf("tamano demo_names: %d \n", sizeof(demo_names)/sizeof(char*) );
     draw_detections(display, demo_detections, demo_thresh, boxes, probs, 0, demo_names, demo_alphabet, demo_classes);
 
     demo_index = (demo_index + 1)%demo_frame;
