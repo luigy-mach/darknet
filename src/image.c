6297df752d6cab94a3e89c114455c44b733aaa3b
luigy-mach <luigy.mach.arc@gmail.com>
2017-12-19 19:02:20 -0500

    update

--- src/image.c
+++ src/image.c
@@ -259,6 +259,8 @@ void draw_detections(image im, int num, float thresh, box *boxes, float **probs,
 
                 //modificacion!!!
                 //printf("%s: %.0f%%\n", names[j], probs[i][j]*100);
+                
+                printf("::::::::::::%s:::::\n", labelstr);
                 char strtemp[]="person";
                 if(0==strcmp(names[j],strtemp))
                     printf("%s: %.0f%%\n", names[j], probs[i][j]*100);
