56af8c4d1743103bbf42d46a5facc9b26897a9d7
luigy-mach <luigy.mach.arc@gmail.com>
2017-12-30 16:04:31 -0500

    update

--- push_git.sh
+++ push_git.sh
@@ -4,4 +4,16 @@
 
 git add .
 git commit -m "update"
-git push origin master
+
+
+
+spawn "git push origin master"
+expect "Username for 'https://github.com':"
+send "luigy-mach"
+
+expect "Password for 'https://luigy-mach@github.com':"
+send "luigy1mach"
+
+
+
+
