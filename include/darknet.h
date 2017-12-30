5e0d62fb1c4cd76ec59307f1bf41b9c8c2d79268
luigy-mach <luigy.mach.arc@gmail.com>
2017-12-30 12:46:52 -0500

    update

--- include/darknet.h
+++ include/darknet.h
@@ -704,7 +704,7 @@ box float_to_box(float *f, int stride);
 void draw_detections(image im, int num, float thresh, box *boxes, float **probs, float **masks, char **names, image **alphabet, int classes);
 void my_draw_detections(image im, int num, float thresh, box *boxes, float **probs, float **masks, char **names, image **alphabet, int classes, int num_frame);
 void my_draw_detections2(image im, int num, float thresh, box *boxes, float **probs, float **masks, char **names, image **alphabet, int classes, int num_frame,FILE *fp);
-#include "tracking.h"
+#include "mytracking.h"
 void my_draw_detections3(image im, int num, float thresh, box *boxes, float **probs, float **masks, char **names, image **alphabet, int classes, int num_frame,FILE *fp,tracking_obj **tracking_array_obj, int tracking_tam_array, double mythreshold);
 
 matrix network_predict_data(network *net, data test);
