344ec4658a6b30c3c4ed01d5794dcf44acf45988
luigy-mach <luigy.mach.arc@gmail.com>
2017-12-30 22:56:30 -0500

    update

--- src/image.c
+++ src/image.c
@@ -886,7 +886,7 @@ void my_draw_detections_list(image im, int num, float thresh, box *boxes, float 
     }
     fprintf(fp, "-----------------------\n");
 
-    update_perdida(demo_list_tracking_obj, num_frame);
+    //update_perdida(demo_list_tracking_obj, num_frame);
 
     //if((num_frame%8)==0){
     //char buffer2[4096]={0};
