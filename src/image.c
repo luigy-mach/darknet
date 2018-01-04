65d2568048893ea377214c48e9f12e58c5d996d1
luigy-mach <luigy.mach.arc@gmail.com>
2018-01-04 09:47:30 -0500

    update

--- src/image.c
+++ src/image.c
@@ -910,7 +910,7 @@ void my_draw_detections_list(image im, int num, float thresh, box *boxes, float 
     //update_perdida funciona-ok 
     update_perdida_v2(demo_list_tracking_obj, num_frame);
 
-    if((num_frame%5)==0){
+    if((num_frame%2)==0){
         printf("num_frame<<<<<<<<<\n");
         update_distancia(demo_list_tracking_obj);
         update_velocidad(demo_list_tracking_obj);
