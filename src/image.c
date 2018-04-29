14fa888defca6e08b6c313f343d2df29d50d9290
luigy-mach <luigy.mach.arc@gmail.com>
2018-04-28 19:45:34 -0500

    update

--- src/image.c
+++ src/image.c
@@ -697,8 +697,8 @@ void my_draw_detections_testv2(image im, detection *dets, int num, float thresh,
     printf("    myTrackingObj_deleteALLBoundingBoxLost \n");
     myTrackingObj_deleteALLBoundingBoxLost(demo_list_tracking_obj);
     printf("    myTrackingObj_printListObjInFile \n");
-    //myTrackingObj_printListObjInFile(*demo_list_tracking_obj,demo_filePointer);    
-    myTrackingObj_printListObjInFile_velocity(*demo_list_tracking_obj,demo_filePointer);
+    myTrackingObj_printListObjInFile(*demo_list_tracking_obj,demo_filePointer);    
+    //myTrackingObj_printListObjInFile_velocity(*demo_list_tracking_obj,demo_filePointer);
     printf("fin my_draw_detections_test 12\n");
 }// fin my_draw_detections_test
 
