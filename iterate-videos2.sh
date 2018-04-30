6c97deecfdd14e6cb79ff30b518c780200c7b8c9
luigy-mach <luigy.mach.arc@gmail.com>
2018-04-30 17:58:00 -0500

    update

--- iterate-videos2.sh
+++ iterate-videos2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for filename in /home/tesista1/yolo/DataVicente/data/fights/*.*; do
+for filename in /home/tesista1/yolo/DataVicente/data/noFights/*.*; do
     ./darknet detector demo -prefix "../video-out-images/$filename" cfg/coco.data cfg/yolov3.cfg ../weights/yolov3.weights $filename
 done
