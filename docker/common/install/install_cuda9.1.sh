17a079420e8acd81c7964ba1400280531e3702e7
luigy-mach <luigy.mach.arc@gmail.com>
2018-12-29 11:58:15 -0500

    docker

--- docker/common/install/install_cuda9.1.sh
+++ docker/common/install/install_cuda9.1.sh
@@ -4,6 +4,7 @@ set -e
 echo "Install Cuda 9.1"
 cd /opt
 wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64
+mv cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64 cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64.deb
 chmod +x cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64.deb
 dpkg -i cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64.deb
 apt-key add /var/cuda-repo-9-1-local/7fa2af80.pub
