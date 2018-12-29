f3c20c18485c64905df5f3e389d9d7bc3023fdbe
luigy-mach <luigy.mach.arc@gmail.com>
2018-12-29 09:40:32 -0500

    update Docker

--- docker/common/install/install_opencv.sh
+++ docker/common/install/install_opencv.sh
@@ -2,13 +2,16 @@
 set -e
 
 echo "Install Opencv 3.3.1"
+wget -O /opt/opencv.zip https://github.com/opencv/opencv/archive/3.3.1.zip
+wget -O /opt/opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.3.1.zip 
 
-git clone --branch 3.3.1 https://github.com/opencv/opencv.git /opt/opencv
-git clone --branch 3.3.1 https://github.com/opencv/opencv_contrib.git /opt/opencv
+cd /opt
+unzip opencv.zip -d ./ && mv opencv-3.3.1 opencv
+unzip opencv_contrib.zip -d ./ && mv opencv_contrib-3.3.1 opencv_contrib
 
-mkdir /opt/opencv/release
-
-cd /opt/opencv/release
+cd opencv
+mkdir release
+cd release
 
 cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
 
